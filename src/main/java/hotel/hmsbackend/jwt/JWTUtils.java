678c673ddfaf72ab4689d2b24071889b4779cfcd
udaya <ping.udaya@gmail.com>
2023-03-09 13:17:12 +0545

    jwt

--- src/main/java/hotel/hmsbackend/jwt/JWTUtils.java
+++ src/main/java/hotel/hmsbackend/jwt/JWTUtils.java
@@ -39,7 +39,6 @@ public class JWTUtils {
                 .setIssuedAt(new Date(System.currentTimeMillis()))
                 .setExpiration(new Date(System.currentTimeMillis()+200*60*60*10))
                 .signWith(SignatureAlgorithm.HS256, secret).compact();
-
     }
     public Boolean validateToken(String token, UserDetails userDetails){
         final String username = extractUsername(token);
--- src/main/java/hotel/hmsbackend/jwt/JwtFilter.java
+++ src/main/java/hotel/hmsbackend/jwt/JwtFilter.java
@@ -1,2 +1,27 @@
-package hotel.hmsbackend.jwt;public class JwtFilter {
+package hotel.hmsbackend.jwt;
+import io.jsonwebtoken.Claims;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+@Component
+public class JwtFilter extends OncePerRequestFilter {
+    @Autowired
+    private JWTUtils jwtUtils;
+    @Autowired
+    CustomerUserDetailsSerivce serivce;
+
+    Claims claims =null;
+    private String username = null;
+    @Override
+    protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, FilterChain filterChain) throws ServletException, IOException {
+    if(httpServletRequest.getServletPath().matches("hms/login | hms/signup | hms/forgetpassword")){
+        filterChain.doFilter(httpServletRequest, httpServletResponse);
+
+    }
+    }
 }
