8b8fedf3e92ecc963bf7bf8f14cfae76e42e9692
udaya <ping.udaya@gmail.com>
2023-03-09 13:38:35 +0545

    jwt

--- src/main/java/hotel/hmsbackend/jwt/JwtFilter.java
+++ src/main/java/hotel/hmsbackend/jwt/JwtFilter.java
@@ -1,6 +1,10 @@
 package hotel.hmsbackend.jwt;
 import io.jsonwebtoken.Claims;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 import javax.servlet.FilterChain;
@@ -8,6 +12,8 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.security.Security;
+
 @Component
 public class JwtFilter extends OncePerRequestFilter {
     @Autowired
@@ -21,7 +27,34 @@ public class JwtFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, FilterChain filterChain) throws ServletException, IOException {
     if(httpServletRequest.getServletPath().matches("hms/login | hms/signup | hms/forgetpassword")){
         filterChain.doFilter(httpServletRequest, httpServletResponse);
+    }else {
+        String authorizationHeader = httpServletRequest.getHeader("Authorization");
+        String token = null;
+        if (authorizationHeader!=null && authorizationHeader.startsWith("Bearer ")){
+            token=authorizationHeader.substring(7);
+            username = jwtUtils.extractUsername(token);
+            claims = jwtUtils.extractAllClaims(token);
+        }
+        if (username!=null && SecurityContextHolder.getContext().getAuthentication()==null){
+            UserDetails userDetails=serivce.loadUserByUsername(username);
+            if (jwtUtils.validateToken(token,userDetails)){
+                UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken =new UsernamePasswordAuthenticationToken(userDetails,null,userDetails.getAuthorities());
+                usernamePasswordAuthenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(httpServletRequest));
+                SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+            }
+        }
+        filterChain.doFilter(httpServletRequest, httpServletResponse);
+    }
 
     }
+    public boolean isAdmin(){
+        return "admin".equalsIgnoreCase((String) claims.get("role"));
+    }
+
+    public boolean isUser(){
+        return "user".equalsIgnoreCase((String) claims.get("role"));
+    }
+    public String getCurrentUser(){
+        return username;
     }
 }
